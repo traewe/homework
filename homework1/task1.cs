310eb24f8f672da36ce1a84a279cfe5d896dab63
Oleksii Kovalenko <alex59.kovalenko@gmail.com>
2022-10-03 14:44:01 +0300

    Перше дз

---
+++ homework1/task1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace Program1
+{
+    class World
+    {
+        static void Main(string[] arg)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int d = 0;
+            Console.WriteLine("Enter Value for a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Value for b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Value for c: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Value for d: ");
+            d = Convert.ToInt32(Console.ReadLine());
+            if (a < b)
+            {
+                if (a < c)
+                {
+                    if (a < d)
+                    {
+                        Console.WriteLine("A is minimim: " + a);
+                    }
+                    else
+                    {
+                        Console.WriteLine("D is minimum: " + d);
+                    }
+                }
+                else
+                {
+                    if (c < d)
+                    {
+                        Console.WriteLine("C is minimim: " + c);
+                    }
+                    else
+                    {
+                        Console.WriteLine("D is minimum: " + d);
+                    }
+                }
+            }
+            else
+            {
+                if (b < c)
+                {
+                    if (b < d)
+                    {
+                        Console.WriteLine("B is minimim: " + b);
+                    }
+                    else
+                    {
+                        Console.WriteLine("D is minimum: " + d);
+                    }
+                }
+                else
+                {
+                    if (c < d)
+                    {
+                        Console.WriteLine("C is minimim: " + c);
+                    }
+                    else
+                    {
+                        Console.WriteLine("D is minimum: " + d);
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ homework1/task2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Program1
+{
+    class World
+    {
+        static void Main(string[] arg)
+        {
+            double x1 = 0;
+            double y1 = 0;
+            double x2 = 0;
+            double y2 = 0;
+            Console.WriteLine("Enter Value for x1: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Value for y1: ");
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Value for x2: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Value for y2: ");
+            y2 = Convert.ToDouble(Console.ReadLine());
+            double x = Math.Pow((x1 - x2), 2);
+            double y = Math.Pow((y1 - y2), 2);
+            double length = Math.Pow((x + y), 0.5);
+            Console.WriteLine("Length = " + length);
+        }
+    }
+}
