14d827e5299f5988a0a0bd5f20a8af6220820873
Oleksii Kovalenko <alex59.kovalenko@gmail.com>
2022-10-16 13:04:01 +0300

    Друге ДЗ

---
+++ homework2/2task1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Program
+{
+    class World
+    {
+        static void Main(string[] args)
+        { 
+            int m = 0;
+            int nfactorial = 1;
+            int mfactorial = 1;
+            int nmfactorial = 1;
+            int counter = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int n = i; n >= counter; counter++)
+                {
+                    for (int j = 1; j <= m; j++)
+                    {
+                        mfactorial *= j;
+                    }
+                    for (int j = 1; j <= n; j++)
+                    {
+                        nfactorial *= j;
+                    }
+                    for (int j = 1; j <= n - m; j++)
+                    {
+                        nmfactorial *= j;
+                    }
+                    Console.Write(nfactorial / (mfactorial * nmfactorial) + " ");
+                    nfactorial = 1;
+                    mfactorial = 1;
+                    nmfactorial = 1;
+                    m++;
+                }
+                counter = 0;
+                m = 0;
+                Console.WriteLine("");
+            }
+        }
+    }
+}
