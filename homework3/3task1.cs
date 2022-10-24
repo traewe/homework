ececaf8f4d64f68be531cc2b03c31afd3b0b5ff9
Oleksii Kovalenko <alex59.kovalenko@gmail.com>
2022-10-24 19:06:25 +0300

    Третє ДЗ

---
+++ homework3/3task1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Program
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            double l = 0, r = 0, e = 1, cub_a = 1, cal_a = 0;
+            string symbol = "";
+            Console.WriteLine("How many numbers after comma do you want?");
+            int num_digits = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            for (int i = 0; i < num_digits; i++)
+            {
+                e /= 10;
+            }
+            if (a < 0)
+            {
+                symbol = "-";
+                a = -a;
+            }
+            if (a >= 1)
+            {
+                l = 1;
+                r = a;
+            }
+            if (0 < a && a < 1)
+            {
+                l = 0;
+                r = 1;
+            }
+            do
+            {
+                cub_a = (l + r) / 2;
+                cal_a = cub_a * cub_a * cub_a;
+                if (cal_a > a)
+                {
+                    r = cub_a;
+                }
+                else
+                {
+                    l = cub_a;
+                }
+            } while (Math.Abs(a - cal_a) > e);
+                Console.WriteLine(String.Format(symbol + "{0:F" + num_digits + "}", cub_a));
+        }
+    }
+}
---
+++ homework3/3task2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+namespace Program
+{
+    class World
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            int counterdots = 0;
+            Console.WriteLine("Enter value for size: ");
+            size = Convert.ToInt32(Console.ReadLine());
+            int counterspace = size * 2 - 3;
+            //Верхня межа
+            Console.Write("#");
+            for (int i = 0; i < size * 4; i++)
+            {
+                Console.Write("=");
+            }
+            Console.WriteLine("#");
+            //Верхня частина
+            for (int i = 0; i < size - 1; i++)
+            {
+                counterdots = 0;
+                for (int k = counterspace; k > 0; k--)
+                {
+                    Console.Write(" ");
+                }
+                counterspace -= 2;
+                Console.Write("| <>");
+                for (int j = 0; j < i * 4; j++)
+                {
+                    Console.Write(".");
+                    counterdots++;
+                }
+                Console.WriteLine("<> |");
+            }
+            //Центр
+            for (int k = counterspace; k > 0; k--)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("|<>");
+            for (int i = 0; i < counterdots + 4; i++)
+            {
+                Console.Write(".");
+            }
+            Console.WriteLine("<>|");
+            for (int k = counterspace; k > 0; k--)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("|<>");
+            for (int i = 0; i < counterdots + 4; i++)
+            {
+                Console.Write(".");
+            }
+            Console.WriteLine("<>|");
+            counterspace++;
+            //Нижня частина
+            for (int i = size - 1; i > 0; i--)
+            {
+                for (int k = counterspace + 1; k > 0; k--)
+                {
+                    Console.Write(" ");
+                }
+                counterspace += 2;
+                Console.Write("| <>");
+                for (int j = 0; j < (i - 1) * 4; j++)
+                {
+                    Console.Write(".");
+                }
+                Console.WriteLine("<> |");
+            }
+            //Нижня межа
+            Console.Write("#");
+            for (int i = 0; i < size * 4; i++)
+            {
+                Console.Write("=");
+            }
+            Console.WriteLine("#");
+        }
+    }
+}
