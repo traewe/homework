604da3ac29da04b6abb426a47b007866e8d3bc83
Oleksii Kovalenko <alex59.kovalenko@gmail.com>
2022-12-06 12:39:59 +0200

    Четверте ДЗ

---
+++ homework4/4task1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Program
+{
+    class World
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(substr_lastindex("abcd", "cd"));
+
+        }
+        static int substr_lastindex(string str1, string str2)
+        {
+            int len1 = str1.Length;
+            int len2 = str2.Length;
+            bool f;
+            for (int i = 0; i < len1 - len2 + 1; i++)
+            {
+                f = true;
+                for (int j = 0; j < len2; j++)
+                {
+                    if (str1[i + j] != str2[j])
+                    {
+                        f = false;
+                        break;
+                    }
+                }
+                if (f)
+                {
+                    return i + len2 - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
