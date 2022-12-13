083f84b67278071238ed625d9aeb0c20b939ea37
Oleksii Kovalenko <alex59.kovalenko@gmail.com>
2022-12-13 17:19:41 +0200

    П'яте ДЗ лекція

---
+++ homework5/5task1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    class Homework
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10};
+            Console.WriteLine(firstindex(array, 10));
+            Console.WriteLine(lastindex(array, 10));
+        }
+        static int firstindex(int[] array, int digit)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+            int first = -1;
+            while (left <= right)
+            {
+                if (first != -1)
+                {
+                    right = first;
+                }
+                int i = (left + right) / 2;
+                if (array[i] == digit)
+                {
+                    first = i;
+                }
+                else if (digit < array[i])
+                {
+                    right = i - 1;
+                }
+                else if (digit > array[i])
+                {
+                    left = i + 1;
+                }
+                if (right == left)
+                {
+                    if (array[right] == digit)
+                    {
+                        return right;
+                    }
+                }
+            }
+            return -1;
+        }
+        static int lastindex(int[] array, int digit)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+            int last = -1;
+            while (left <= right)
+            {
+                if (last != -1)
+                {
+                    left = last;
+                }
+                int i = (left + right) / 2;
+                if (array[i] == digit)
+                {
+                    last = i;
+                }
+                else if (digit < array[i])
+                {
+                    right = i - 1;
+                }
+                else if (digit > array[i])
+                {
+                    left = i + 1;
+                }
+                if (right == left || left == right - 1)
+                {
+                    if (array[right] == digit)
+                    {
+                        return right;
+                    }
+                    else if (array[left] == digit)
+                    {
+                        return left;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ homework5/5task2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Program
+{
+    class Homework
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 0, 1, 3, 5, 7, 7, 7, 6, 4, 2, -1};
+            Console.WriteLine(Binarysearch(array, -1));
+        }
+        static int Binarysearch(int[] array, int digit)
+        {
+            int top = Array.LastIndexOf(array, array.Max());
+            int left = 0;
+            int right = top;
+            while (left <= right)
+            {
+                int i = (left + right) / 2;
+                if (array[i] == digit)
+                {
+                    return i;
+                }
+                else if (array[i] < digit)
+                {
+                    left = i + 1;
+                }
+                else if (array[i] > digit)
+                {
+                    right = i - 1;
+                }
+            }
+            left = top + 1;
+            right = array.Length - 1;
+            while (left <= right)
+            {
+                int i = (left + right) / 2;
+                if (array[i] == digit)
+                {
+                    return i;
+                }
+                else if (array[i] < digit)
+                {
+                    right = i - 1;
+                }
+                else if (array[i] > digit)
+                {
+                    left = i + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
